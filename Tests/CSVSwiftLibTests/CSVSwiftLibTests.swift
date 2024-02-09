3899fc00423e772b023d2b06ee150a86bb735983
Matt Hogg <matt.hogg74@hotmail.com>
2024-02-09 23:16:06 +0000

    Added tests

--- Tests/CSVSwiftLibTests/CSVSwiftLibTests.swift
+++ Tests/CSVSwiftLibTests/CSVSwiftLibTests.swift
@@ -2,11 +2,25 @@ import XCTest
 @testable import CSVSwiftLib
 
 final class CSVSwiftLibTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func testSimple() throws {
+        let csv = "H1,H2\nPart a,Part b"
+		let res = CSVTranslation.getArrayFromCSVContent(csv: csv)
+		XCTAssert(res.count == 2)
+		res.forEach { XCTAssert($0.count == 2) }
+		XCTAssert(res[0][0] == "H1")
+		XCTAssert(res[0][1] == "H2")
+		XCTAssert(res[1][0] == "Part a")
+		XCTAssert(res[1][1] == "Part b")
     }
+	
+	func testQuoted() throws {
+		let csv = "H1,\"Heading, 2\"\n\"This, is some data\",this is the second part"
+		let res = CSVTranslation.getArrayFromCSVContent(csv: csv)
+		XCTAssert(res.count == 2)
+		res.forEach { XCTAssert($0.count == 2) }
+		XCTAssert(res[0][0] == "H1")
+		XCTAssert(res[0][1] == "Heading, 2")
+		XCTAssert(res[1][0] == "This, is some data")
+		XCTAssert(res[1][1] == "this is the second part")
+	}
 }
